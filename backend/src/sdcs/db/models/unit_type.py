25e6c88c38ae1b54c491580441cf73422904eb7d
MartinCo <martin@hatchlane.com>
2024-03-11 20:39:14 +0000

    Backend: Update DCSUnitType association

--- backend/src/sdcs/db/models/unit_type.py
+++ backend/src/sdcs/db/models/unit_type.py
@@ -1,10 +1,18 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, Text, Enum
+from sqlalchemy.orm import relationship
+
 from sdcs.db import SQLBase
 from .unit_class import UnitClass
+
+class DCSUnitType(SQLBase):
+    id = Column(Integer, primary_key=True)
+    type_name = Column(Text, nullable=False, unique=True)
 
 class UnitType(SQLBase):
     id = Column(Integer, primary_key=True)
     type_name = Column(Text, nullable=False, unique=True)
     is_static = Column(Boolean)
-    unit_type = Column(Text)
     unit_class = Column(Enum(UnitClass))
+
+    dcs_type_id = Column(Integer, ForeignKey('dcs_unit_type.id'))
+    dcs_type = relationship('DCSUnitType')
